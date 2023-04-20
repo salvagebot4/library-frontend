a86853f56a694f3522800504cfdeeeca074eaefb
salvagebot4 <131232386+salvagebot4@users.noreply.github.com>
2023-04-20 16:27:20 -0500

    homepage route added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import * as Pages from './pages';
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
+      <Routes>
+        {/* Public Routes */}
+        <Route exact path="/" element={<Pages.Home/>}/>
 
-        <h1>The Book Nook</h1>
-
-      </header>
-    </div>
+      </Routes>
+    </>
   );
 }
 
