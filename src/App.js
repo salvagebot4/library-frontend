c9ff277ef4ca452609491887f94dede50399a19a
rrizvi3 <128552797+rrizvi3@users.noreply.github.com>
2023-04-21 18:00:37 -0500

    added books page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Route exact path="/about" element={<Pages.About/>}/>
         <Route exact path="/products" element={<Pages.Products/>}/>
         <Route exact path="/users" element={<Pages.Users/>}/>
+        <Route exact path="/books" element={<Pages.Books/>}/>
       </Routes>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as Home } from './Home/Home';
 export { default as About } from './About/About';
 export { default as Products } from './Products/Products';
 export { default as Users } from './Users/Users';
+export { default as Books } from './Books/Books';
